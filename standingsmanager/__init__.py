0277e3202977c23e6e39dd7ca8a98748fe839451
Guarzo <guarzo.eve@gmail.com>
2026-04-13 02:40:26 -0400

    fix: yep

--- standingsmanager/__init__.py
+++ standingsmanager/__init__.py
@@ -3,5 +3,5 @@
 # pylint: disable = invalid-name
 default_app_config = "standingsmanager.apps.StandingsManagerConfig"
 
-__version__ = "0.0.17"
+__version__ = "0.0.18"
 __title__ = "Standings Manager"
